ce1679e2e89190bfc68af5b95378322699939537
ETJeanMachine <ericjean873@gmail.com>
2022-01-18 16:31:22 -0500

    implemented directing to login page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,25 @@
 import React from "react";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Home(props) {
-  return <div className="Home">Welcome Home!</div>;
+  if (props.userName != null) {
+    return (
+      <>
+        <div className="Taskbar">
+          Logged in as {props.userName} - <Link to="/home">Log Out</Link>
+        </div>
+        <div className="Home">Logged in.</div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="Taskbar">
+        <Link to="/login">Log In</Link>
+      </div>
+      <div className="Home">Not Logged In</div>
+    </>
+  );
 }
 
 export default Home;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return <div className="Login">Login.</div>;
 }
 
-export default Login
-
+export default Login;
