3b00d67caca078a112536eb28f437f9cecfa4c8d
ETJeanMachine <ericjean873@gmail.com>
2022-01-19 20:55:54 -0500

    changed to anime; mal's API is easier than IMDB's

---
+++ src/components/Anime.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Anime() {
+  return (
+    <div>
+      <h3>ANIME</h3>
+      <input type="search" placeholder="Search..." />
+    </div>
+  );
+}
+
+export default Anime;
