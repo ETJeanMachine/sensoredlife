6b93f89a616ea23e1e8ee975668ad55a8c16fda6
ETJeanMachine <ericjean873@gmail.com>
2022-01-19 21:24:46 -0500

    implemented anime search (a bit buggy on the api, may change)

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,10 +1,42 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import { format } from "react-string-format";
 
 function Anime() {
+  const [anime, setAnime] = useState([]);
+  const [query, setQuery] = useState(null);
+
+  useEffect(() => {
+    search();
+  }, []);
+
+  const search = async (key) => {
+    setQuery(key);
+    console.log(query);
+    const url = format("https://api.jikan.moe/v4/anime?q={0}&sfw&limit=5", key);
+    const response = await fetch(url);
+    const info = await response.json();
+    try {
+      setAnime(info.data);
+      console.log(info);
+      console.log(anime);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div>
       <h3>ANIME</h3>
-      <input type="search" placeholder="Search..." />
+      <input
+        type="search"
+        placeholder="Search..."
+        onChange={(event) => search(event.target.value)}
+      />
+      {anime && anime.length > 0 && query ? (
+        anime.map((ani) => <li>{ani.title}</li>)
+      ) : (
+        <h4>No Anime found</h4>
+      )}
     </div>
   );
 }
