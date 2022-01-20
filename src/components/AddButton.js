d9bd268d507ec2039c6a82227cc2df7eaab57c4d
ETJeanMachine <ericjean873@gmail.com>
2022-01-20 13:44:11 -0500

    button for adding info to database

---
+++ src/components/AddButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc, setDoc, query, where } from 'firebase/firestore';
+
+
+function AddButton(props) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBrfqL3Daal65Ol1CaXt6AlZ2SM3j0rOUs",
+    authDomain: "sensored-life.firebaseapp.com",
+    projectId: "sensored-life",
+    storageBucket: "sensored-life.appspot.com",
+    messagingSenderId: "985352494852",
+    appId: "1:985352494852:web:fb8e5c865c7c2f58dd7a51",
+    measurementId: "G-TWKR6RBNE4",
+  };
+  const { user } = useAuth0();
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  
+  async function addToDb() {
+    
+  }
+  
+  return <button onClick={() => addToDb()}>+</button>;
+}
+
+export default AddButton;
